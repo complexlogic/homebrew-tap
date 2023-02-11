3e9d5a671517c24b70a1c8e08ba0ee34ff18ca16
complexlogic <95071366+complexlogic@users.noreply.github.com>
2023-02-11 00:14:28 +0000

    Update

--- Formula/rsgain.rb
+++ Formula/rsgain.rb
@@ -1,9 +1,9 @@
 class Rsgain < Formula
   desc "A simple, but powerful ReplayGain 2.0 tagging utility"
   homepage "https://github.com/complexlogic/rsgain"
-  version "3.1.1"
+  version "3.2"
   url "https://github.com/complexlogic/rsgain/releases/download/v#{version}/rsgain-#{version}-source.tar.xz"
-  sha256 "0c9efdf7433943f2e3674a6859eb72c80fede333b5e86151eaa0492e83923d5a"
+  sha256 "179d0a132bcc8d64e354ef41495409d517b2b22fecbea64672e2fcdfe6dfe3d4"
   license "BSD-2-Clause"
 
   depends_on "cmake" => :build
